8f2d1018dad87d540c6836ceaa9d9376e1d922c5
Ashok734 <meashok850@gmail.com>
2022-05-02 23:02:39 -0700

    Add all my files

--- js/main.js
+++ js/main.js
@@ -14,33 +14,216 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
-
-
-$('.main-slider').owlCarousel({
-  loop: true,
-  margin: 10,
-  dots: false,
-  nav: true,
-  mouseDrag: false,
-  autoplay: true,
-  animateOut: 'fadeOut',
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 1
-    },
-    1000: {
-      items: 1
-    }
+// hide and show password===========
+$(".toggle-password").click(function () {
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
   }
 });
-// Counter
-$('.counter').counterUp({
-  delay: 10,
-  time: 2000
-});
-$('.counter').addClass('animated fadeInDownBig');
-$('h3').addClass('animated fadeIn');
+// SIGN UP TYPEWITER
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #447d02;}";
+  document.body.appendChild(css);
+};
+// SIGN FORM VELIDATION===========
+(function () {
+  'use strict'
+  var forms = document.querySelectorAll('.needs-validation')
+  Array.prototype.slice.call(forms)
+    .forEach(function (form3) {
+      form3.addEventListener('submit', function (event) {
+        if (!form3.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form3.classList.add('was-validated')
+      }, false)
+    })
+})()
+// VERIFY YOUR NUMBER FOR SIGN UP
+
+$(document).ready(function () {
+  if (localStorage.getItem('popState') != 'shown') {
+    $("#myModal").delay(2000).fadeIn();
+    localStorage.setItem('popState', 'shown')
+  }
+  $('#myModal').modal('show');
+});
+
+// SIGN UP NEXT FUNCTION=============================================================
+$(function () {
+
+  //jQuery time
+  var current_fs, next_fs, previous_fs; //fieldsets
+  var left, opacity, scale; //fieldset properties which we will animate
+  var animating; //flag to prevent quick multi-click glitches
+
+  $(".next").click(function () {
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    //activate next step on progressbar using the index of next_fs
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({ opacity: 0 }, {
+      step: function (now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale current_fs down to 80%
+        scale = 1 - (1 - now) * 0.2;
+        //2. bring next_fs from the right(50%)
+        left = (now * 50) + "%";
+        //3. increase opacity of next_fs to 1 as it moves in
+        opacity = 1 - now;
+        current_fs.css({ 'transform': 'scale(' + scale + ')' });
+        next_fs.css({ 'left': left, 'opacity': opacity });
+      },
+      duration: 1,
+      complete: function () {
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: 'easeInOutBack'
+    });
+  });
+
+  $(".previous").click(function () {
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+
+    //de-activate current step on progressbar
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+    //show the previous fieldset
+    previous_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({ opacity: 0 }, {
+      step: function (now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale previous_fs from 80% to 100%
+        scale = 0.8 + (1 - now) * 0.2;
+        //2. take current_fs to the right(50%) - from 0%
+        left = ((1 - now) * 50) + "%";
+        //3. increase opacity of previous_fs to 1 as it moves in
+        opacity = 1 - now;
+        current_fs.css({ 'left': left });
+        previous_fs.css({ 'transform': 'scale(' + scale + ')', 'opacity': opacity });
+      },
+      duration: 1,
+      complete: function () {
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: 'easeInOutBack'
+    });
+  });
+
+  $(".submit").click(function () {
+    return false;
+  })
+
+});
+
+
+// FOR events ==========
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:2,
+  dots:false,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:8
+      }
+  }
+})
+// fixed event top
+document.addEventListener("DOMContentLoaded", function () {
+
+  window.addEventListener('scroll', function () {
+
+    if (window.scrollY > 200) {
+      document.getElementById('navbar_top-menu').classList.add('fixed-top');
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+    } else {
+      document.getElementById('navbar_top-menu').classList.remove('fixed-top');
+      // remove padding top from body
+      document.body.style.paddingTop = '0';
+    }
+  });
+});
